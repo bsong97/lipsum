1baf0d28e50d6fcf340165f597276e4c866197e4
bsong <bsong97@gmail.com>
2013-05-03 08:48:11 +0800

    delete replicated files

--- lipsum/README.md
+++
@@ -1,1 +0,0 @@
-A project template for running a Flask-based application on Google App Engine with some static html and a contact form
--- lipsum/page/models.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import db
-
-class Message(db.Model):
-    content = db.TextProperty(required=True)
-    when = db.DateTimeProperty(auto_now_add = True)
